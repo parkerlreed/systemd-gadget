b30897e796ed2bd727220984ab69dc7e88c0abd7
parkerlreed <parker.l.reed@gmail.com>
2023-01-16 15:58:53 -0500

    Create gadget-install.sh

---
+++ gadget-install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+mkdir -p /etc/systemd/scripts
+cp functions.sh gadget-init.sh gadget-start.sh /etc/systemd/scripts/
+mkdir -p /etc/systemd/network
+cp *.net* /etc/systemd/network/
+cp *.service /etc/systemd/system/
