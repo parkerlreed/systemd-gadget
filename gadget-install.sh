66da894bb02dd51ad512abfeed41b45528334875
parkerlreed <parker.l.reed@gmail.com>
2023-01-16 16:36:40 -0500

    Woops

--- gadget-install.sh
+++ gadget-install.sh
@@ -4,3 +4,4 @@ cp functions.sh gadget-init.sh gadget-start.sh /etc/systemd/scripts/
 mkdir -p /etc/systemd/network
 cp *.net* /etc/systemd/network/
 cp *.service /etc/systemd/system/
+cp gadget /etc/default/
